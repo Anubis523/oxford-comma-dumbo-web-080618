9b724c89e72a8d5da31692126f1ff1dca510376e
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 14:33:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   # array.join(',')
   if (array.size == 2)
-    array.join('and')
+    array.join('and ')
   elsif (array.size > 2)
     array.shift+", "+oxford_comma(array)
   end
