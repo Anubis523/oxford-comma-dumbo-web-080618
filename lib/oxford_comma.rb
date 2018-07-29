78dbdda01d07f65149c1cd5ff74a2c59b3f4d0e9
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 14:47:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,6 @@ def oxford_comma(array)
     array[0].to_s
   elsif (array.size == 2)
     array.join(' and ')
-  elsif (array.size > 2)
-    element = array.shift
-    if (array.size == 2)
-      "#{element}, #{array.shift}, and #{array.shift}"
-    end
-    element+", "+oxford_comma(array)
+  elsif (array.size-1 == 2 )
   end
 end
