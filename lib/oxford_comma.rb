db334efcd075272f07d156878348803d0107d607
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 14:35:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if (array.size == 1)
     array[0].to_s
   elsif (array.size == 2)
-    array.join('and ')
+    array.join(', and ')
   elsif (array.size > 2)
     array.shift+", "+oxford_comma(array)
   end
