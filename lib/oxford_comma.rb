f06909a4f90364c2b5d606a165f5c825a4445bf0
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 14:45:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   elsif (array.size > 2)
     element = array.shift
     if (array.size == 2)
-      element + ", and "
+      "#{element}, #{array.shift}, and #{array.shift}"
     end
     element+", "+oxford_comma(array)
   end
