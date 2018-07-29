03441c5dbd4c10f81d2e8a2c76942b6ed4f22bf3
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 14:41:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   # array.join(',')
   if (array.size == 1)
     array[0].to_s
-  elsif (array.size == 2)
+  elsif (array.size == 2 && array[0].index(',') != array[0].rindex(','))
     array.join(', and ')
   elsif (array.size > 2)
     array.shift+", "+oxford_comma(array)
